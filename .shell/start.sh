062b78478e6bcc29e57cb11da4d13a366e6a3c59
risfeng <risfeng@126.com>
2020-04-06 13:49:40 +0800

    ssh exce command.

--- .shell/start.sh
+++ .shell/start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -jar adc.ms.eureka.jar -Djava.security.egd=file:/dev/./urandom &
+echo ${{ secrets.TENCENT_IMAGES_HUB_TOKEN }} | docker login --username=${{ secrets.TENCENT_IMAGES_HUB_USER_NAME }} ${{ secrets.TENCENT_IMAGES_HUB_REGISTRY_URL }} --password-stdin
+docker pull ${{ steps.build_push_image.outputs.image_pull_url }}"
