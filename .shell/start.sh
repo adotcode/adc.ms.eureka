72a495eed8f1e4b9c5e76b6d917793073e6ce7fa
risfeng <risfeng@126.com>
2020-04-06 15:23:55 +0800

    ssh exce command.

--- .shell/start.sh
+++
@@ -1,2 +0,0 @@
-echo ${{ secrets.TENCENT_IMAGES_HUB_TOKEN }} | docker login --username=${{ secrets.TENCENT_IMAGES_HUB_USER_NAME }} ${{ secrets.TENCENT_IMAGES_HUB_REGISTRY_URL }} --password-stdin
-docker pull ${{ steps.build_push_image.outputs.image_pull_url }}
